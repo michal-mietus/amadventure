6e32aa4e550b4f34807ee2cc2e47ced49d205392
goodude123 <assasinmcpl@gmail.com>
2019-03-09 20:21:24 +0100

    Tests for Ability model.

---
+++ hero_upgrade_system/tests/test_models/test_ability.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from ...models.ability import Ability
+from ...models.occupation import Occupation
+from ...models import abilities
+
+
+class TestAbility(TestCase):
+    def setUp(self):
+        self.occupation = Occupation.objects.create(
+            name=Occupation.WARRIOR,
+        )
+
+    def test_create_ability(self):
+        ability = Ability(
+            name='charge',
+            occupation=self.occupation,
+            parent=None,
+            unblock_level=0,
+            level=1,
+            category=Ability.ACTIVE,
+        )
+
+        # ability module = what with that?
+        # function name = can be a string
