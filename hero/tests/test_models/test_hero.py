1fd75446ce6658e194a86345590d5ced155ea3f4
goodude123 <assasinmcpl@gmail.com>
2019-03-09 12:25:48 +0100

    Creating hero and his representation method tests.

--- hero/tests/test_models/test_hero.py
+++ hero/tests/test_models/test_hero.py
@@ -1,7 +1,25 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from ...models.hero import Hero
 
 
-class HeroTestCase(TestCase):
-    def setUp(self):
-        pass
+class TestHero(TestCase):
+    def create_user(self):
+        user = User.objects.create(
+            username='username',
+            password='password'
+        )
+        return user
+
+    def create_hero(self, name):
+        user = self.create_user()
+        hero = Hero.objects.create(
+            user=user,
+            name=name,
+        )
+        return hero
+
+    def test_hero_string_representation(self):
+        hero_name = 'hero'
+        hero = self.create_hero(hero_name)
+        self.assertEqual(str(hero), 'Hero ' + hero_name)
