3bee5b3d39ba3247cce4341a17073446896a4747
goodude123 <assasinmcpl@gmail.com>
2019-03-09 14:01:44 +0100

    Constant statistic names added to model.

--- hero/models/statistics.py
+++ hero/models/statistics.py
@@ -3,7 +3,12 @@ from .hero import Hero
 
 
 class Statistic(models.Model):
-    name = models.CharField(max_length=35)
+    STATISTICS = (
+        ('strength', 'strength'),
+        ('agility', 'agility'),
+        ('intelligence', 'intelligence'),
+    )
+    name = models.CharField(max_length=35, choices=STATISTICS) # how does it works in view/form ?
     points = models.PositiveIntegerField()
     hero = models.ForeignKey(
         Hero,
