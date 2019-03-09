12f3b21054e9ba97ad2786b86f843bb3e5354b20
goodude123 <assasinmcpl@gmail.com>
2019-03-09 12:24:59 +0100

    Hero model:  removed stats field,  renamed owner to user,  added string representation method.

--- hero/models/hero.py
+++ hero/models/hero.py
@@ -1,16 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .statistics import Statistic
 
 
 class Hero(models.Model):
-    owner = models.OneToOneField(
-        Place,
+    user = models.OneToOneField(
+        User,
         on_delete=models.CASCADE,
     )
     name = models.CharField(max_length=35)
-    stats = models.ForeignKey(
-        Statistic,
-        on_delete=models.CASCADE,
-    )
+
+    def __str__(self):
+        return 'Hero ' + self.name 
+
     ## skills = upgrade_sys.upgrade__skills.all (or one to one?)
