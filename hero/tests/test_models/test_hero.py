2159f05069963bbc56e65d639dc790cbe3b5a436
goodude123 <assasinmcpl@gmail.com>
2019-03-09 12:00:31 +0100

    Initialized project and 3 apps:  - hero,  - hero_upgrade_system,  - user.

---
+++ hero/tests/test_models/test_hero.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+
+class HeroTestCase(TestCase):
+    def setUp(self):
+        pass
---
+++ hero_upgrade_system/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HeroUpgradeSystemConfig(AppConfig):
+    name = 'hero_upgrade_system'
