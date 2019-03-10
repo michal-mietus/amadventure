bbb99284e8de60e35548ea392a3af04565ad203d
goodude123 <assasinmcpl@gmail.com>
2019-03-10 19:08:49 +0100

    Removed is_blocked method tests.

--- hero_upgrade_system/tests/test_models/test_ability.py
+++ hero_upgrade_system/tests/test_models/test_ability.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from ...models.ability import Ability
 from ...models.occupation import Occupation
-from ...models import abilities
+from ...models import occupations
 
 
 class TestAbility(TestCase):
@@ -17,7 +17,6 @@ class TestAbility(TestCase):
             occupation=self.occupation,
             parent=None,
             unblock_level=0,
-            level=level,
             category=Ability.ACTIVE,
             function='charge',
         )
@@ -29,35 +28,5 @@ class TestAbility(TestCase):
             occupation=self.occupation,
             parent=None,
             unblock_level=0,
-            level=1,
             category=Ability.ACTIVE,
         )
-
-        # ability module = what with that?
-        # function name = can be a string
-
-    def test_ablity_is_blocked(self):
-        """Parent ability level is lower than child unlock level"""
-        parent_ability = self.create_ability('charge', 2)
-        child_ability = Ability.objects.create(
-            name='super charge',
-            occupation=self.occupation,
-            parent=parent_ability,
-            unblock_level=3,
-            level=0,
-            category=Ability.ACTIVE
-        )
-        self.assertEqual(child_ability.is_blocked(), True)
-
-    def test_ablity_is_unblocked(self):
-        """Parent ability level is equal to child unlock level"""
-        parent_ability = self.create_ability('charge', 3)
-        child_ability = Ability.objects.create(
-            name='super charge',
-            occupation=self.occupation,
-            parent=parent_ability,
-            unblock_level=3,
-            level=0,
-            category=Ability.ACTIVE
-        )
-        self.assertEqual(child_ability.is_blocked(), False)
