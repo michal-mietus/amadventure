28453734aac7a764f63c2678bbec4d4344118e2a
goodude123 <assasinmcpl@gmail.com>
2019-03-11 18:24:04 +0100

    Statistics creating test.

---
+++ hero_upgrade_system/tests/test_models/test_statistic.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from hero.models.hero import Hero
+from ...models.occupation import Occupation
+from ...models.statistics import Statistic
+
+
+class TestStatistic(TestCase):
+
+    def create_user(self, username):
+        return User.objects.create(
+            username=username,
+            password='zaq1@WSX'
+        )
+
+    def create_hero(self):
+        return Hero.objects.create(
+            user=self.create_user('user1'),
+            name='hero1',
+            occupation=self.create_occupation()
+        )
+
+    def create_occupation(self):
+        return Occupation.objects.create(
+            name=Occupation.WARRIOR,
+            module=Occupation.WARRIOR_MODULE,
+        )
+
+    def test_create_statistic_object(self):
+        Statistic.object.create(
+            name=Statistic.STRENGTH,
+            points=1,
+            hero=self.create_hero()
+        )
