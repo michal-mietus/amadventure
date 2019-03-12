41abdca728d2cfd26493cfb7abdf2cd6d80db604
goodude123 <assasinmcpl@gmail.com>
2019-03-12 15:50:00 +0100

    Added urls for:  main view (after logging)  change statistics, abilities (after upgrade or while creating hero)

--- hero/urls.py
+++ hero/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'hero'
 
 urlpatterns = [
+    path('main/', views.MainView.as_view(), name='main'),
     path('create/', views.HeroCreateView.as_view(), name='hero_create'),
+    path('change/statistics/', views.StatisticsChangeView.as_view(), name='statistics_change'),
+    path('change/abilities/', views.AbilitiesChangeView.as_view(), name='abilities_change'),
 ]
