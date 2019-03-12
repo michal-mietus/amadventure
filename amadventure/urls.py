3237a191eb3e19df22c6b24ab9d65f981ae75620
goodude123 <assasinmcpl@gmail.com>
2019-03-12 15:47:41 +0100

    User app urls included.

--- amadventure/urls.py
+++ amadventure/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hero import urls as hero_urls
+from user import urls as user_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hero/', include(hero_urls)),
+    path('user/', include(user_urls)),
 ]
