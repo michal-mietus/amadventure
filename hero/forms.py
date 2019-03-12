4eece5b32761263c5bfca3b15c4c3f7bb557055f
goodude123 <assasinmcpl@gmail.com>
2019-03-12 15:49:08 +0100

    3 independent views.

--- hero/forms.py
+++ hero/forms.py
@@ -2,14 +2,16 @@ from django import forms
 from hero_upgrade_system.models.occupation import Occupation
 
 
-class HeroUpgradeForm(forms.Form):
+class HeroCreateForm(forms.Form):
+    name = forms.CharField(max_length=35)
+    occupation = forms.ChoiceField(choices=Occupation.OCCUPATIONS)
+
+
+class StatisticsChangeForm(forms.Form):
     strength = forms.IntegerField(min_value=1, initial=1)
     agility = forms.IntegerField(min_value=1, initial=1)
     intelligence = forms.IntegerField(min_value=1, initial=1)
 
 
-class HeroCreateForm(HeroUpgradeForm):
-    name = forms.CharField(max_length=35)
-    occupation = forms.ChoiceField(choices=Occupation.OCCUPATIONS)
-
-    field_order = ['name', 'occupation']
+class AbilitiesChangeForm(forms.Form):
+    pass
