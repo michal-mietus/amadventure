7ed4992afcda5414156640ef5943962e44dc4c25
goodude123 <assasinmcpl@gmail.com>
2019-03-13 20:38:06 +0100

    Decorator which deny user access to website and redirect him to main view from hero app.

---
+++ hero/decorators.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import reverse
+from .models.hero import Hero
+
+
+def deny_access_user_with_hero(function):
+    def wrapper(request, *args, **kwargs):
+        heroes = Hero.objects.filter(user=request.user)
+        if heroes:
+            return HttpResponseRedirect(reverse('hero:main'))
+        return function(request, *args, **kwargs)
+    return wrapper
