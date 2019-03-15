497371dd267211f16ba672546a0d1f8cd78a9036
goodude123 <assasinmcpl@gmail.com>
2019-03-15 15:14:59 +0100

    Hero required decorator, which allow user to access view only if he has created hero.

--- hero/decorators.py
+++ hero/decorators.py
@@ -10,3 +10,12 @@ def deny_access_user_with_hero(function):
             return HttpResponseRedirect(reverse('hero:main'))
         return function(request, *args, **kwargs)
     return wrapper
+
+
+def hero_required(function):
+    def wrapper(request, *args, **kwargs):
+        heroes = Hero.objects.filter(user=request.user)
+        if not heroes:
+            return HttpResponseRedirect(reverse('hero:hero_create'))
+        return function(request, *args, **kwargs)
+    return wrapper
