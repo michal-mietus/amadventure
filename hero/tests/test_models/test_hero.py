6bde62517cc0c86d484892e58da616ba7909c6b0
goodude123 <assasinmcpl@gmail.com>
2019-03-15 15:17:50 +0100

    Tests for hero model.

--- hero/tests/test_models/test_hero.py
+++ hero/tests/test_models/test_hero.py
@@ -1,13 +1,37 @@
 from django.test import TestCase
 from .set_ups import UserAndHeroSetUp
+from hero_upgrade_system.models.statistics import Statistic
 from ...models.hero import Hero
 
 
 class TestHero(UserAndHeroSetUp):
+    def setUp(self):
+        super().setUp()
+        self.hero_name = 'hero'
+        self.hero = self.create_hero('hero')
+
     def test_hero_string_representation(self):
-        hero_name = 'hero'
-        occupation = self.create_occupation()
-        hero = self.create_hero(
-            hero_name
-        )
-        self.assertEqual(str(hero), 'Hero ' + hero_name)
+        self.assertEqual(str(self.hero), 'Hero ' + self.hero_name)
+
+    def test_get_all_stats(self):
+        self.assertQuerysetEqual(self.hero.get_all_stats(), self.hero.statistic_set.all())
+
+    def test_upgrade_statistics(self):
+        statistics = {
+            'strength': 5,
+            'intelligence': 5,
+            'agility': 5,
+        }
+        self.create_hero_statistics(statistics)
+        self.hero.upgrade_statistics(**statistics)
+
+        for name, points in statistics.items():
+            self.assertEqual(points*2, self.hero.statistic_set.get(name=name).points)
+
+    def create_hero_statistics(self, statistics):
+        for name, points in statistics.items():
+            Statistic.objects.create(
+                name=name, 
+                points=points,
+                hero=self.hero
+            )
