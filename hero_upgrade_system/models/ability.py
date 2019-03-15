773174e25637cb721c3e7fc4cd352ddfa398dfa0
goodude123 <assasinmcpl@gmail.com>
2019-03-15 18:06:21 +0100

    HeroAbilirt get parent method  and str representation added.

--- hero_upgrade_system/models/ability.py
+++ hero_upgrade_system/models/ability.py
@@ -1,4 +1,5 @@
 from django.db import models
+from hero.models.hero import Hero
 from .occupation import Occupation
 
 
@@ -15,6 +16,7 @@ class Ability(models.Model):
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
     unblock_level = models.PositiveIntegerField()
     category = models.CharField(max_length=35, choices=CATEGORIES)
+    # probably can be deleted, but have to name functions just as Ability
     function = models.CharField(max_length=100) # occupation.module.function
 
     def __str__(self):
@@ -22,12 +24,22 @@ class Ability(models.Model):
 
 
 class HeroAbility(models.Model):
+    hero = models.ForeignKey(Hero, on_delete=models.CASCADE)
     level = models.PositiveIntegerField(default=0)
     ability = models.ForeignKey(Ability, on_delete=models.CASCADE)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        string_representation = self.hero.name + ' ' + self.ability.name
+        return string_representation
+        
 
     def is_blocked(self):
         """Parent level must be equal or bigger than unblock level."""
         if self.parent.level >= self.ability.unblock_level:
             return False
         return True
+
+    def get_parent(self):
+        parent_ability = self.ability.parent
+        return HeroAbility.objects.get(ability=parent_ability)
