aaf1c9f9de01991e850b49a94fc68d1c42ee152b
goodude123 <assasinmcpl@gmail.com>
2019-03-17 18:33:18 +0100

    Moved some logic to view, added method to sum all of hero ability points.

--- hero/models/hero.py
+++ hero/models/hero.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from hero_upgrade_system.models.occupation import Occupation
-from hero_upgrade_system.models.ability import Ability
 
 
 class Hero(models.Model):
@@ -14,6 +13,7 @@ class Hero(models.Model):
     name = models.CharField(max_length=35)
     occupation = models.ForeignKey(Occupation, on_delete=models.CASCADE)
     statistic_points = models.PositiveIntegerField(default=15)
+    # discrepancy of naming (in ability its Level)
     ability_points = models.PositiveIntegerField(default=3)
 
     def __str__(self):
@@ -38,19 +38,8 @@ class Hero(models.Model):
             points_sum += statistic.points
         return points_sum
 
-    def create_new_hero_abilities(self):
-        """Callled when user is creating hero. """
-        hero_occupation = self.occupation
-        for ability in self.get_all_occuppation_abilities():
-            parent_ability = self.get_hero_ability_parent(ability)
-            HeroAbility.objects.create(
-                hero=hero,
-                ability=ability,
-                parent=parent_ability,
-            )
-        
-    def get_all_occuppation_abilities(self):
-        return Ability.objects.filter(occupation=self.occupation)
-
-    def get_hero_ability_parent(self, ability):
-        return HeroAbility.objects.get(name=ability.parent.name, hero=self)
+    def sum_all_ability_points(self):
+        points_sum = self.ability_points
+        for ability in self.heroability_set.all():
+            points_sum += ability.level
+        return points_sum
