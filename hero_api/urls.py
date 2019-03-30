058b75aadaa05b1664f6e79e601d64cf7e549791
goodude123 <assasinmcpl@gmail.com>
2019-03-31 00:08:10 +0100

    Routes for: AllHeroDataViewSet which returns all serialized data connected with hero model, occupation, which returns all ability for given occupation and view for HeroCreating.

--- hero_api/urls.py
+++ hero_api/urls.py
@@ -7,11 +7,14 @@ app_name = 'hero_api'
 
 router = routers.DefaultRouter()
 router.register('hero', views.HeroViewSet)
+router.register('owned', views.HeroAllDataViewSet, basename="hero_all_data")
 router.register('hero/(?P<hero_pk>[0-9]+)/hero_ability', views.HeroAbilityViewSet)
 router.register('hero/(?P<hero_pk>[0-9]+)/statistic', views.HeroStatisticViewSet)
 router.register('ability', views.AbilityViewSet)
 router.register('occupation', views.OccupationViewSet)
+router.register('occupation/(?P<occupation_pk>[0-9]+)/ability/all', views.OccupationAbilityViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('create/', views.HeroCreateView.as_view(), name='hero_create'),
 ]
