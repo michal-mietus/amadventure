78e12432177583cfd7d73ee5df8bce9a5606e178
goodude123 <assasinmcpl@gmail.com>
2019-04-13 15:42:17 +0200

    Api Artifical urls.

---
+++ artifical/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+
+app_name = 'artifical_api'
+
+router = routers.DefaultRouter()
+router.register('location', views.LocationViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('expedition/<location_id>/', views.ExpeditionView.as_view())
+]
