1a24443080c8bee6dd9b31200a8ef62ce107eb99
goodude123 <assasinmcpl@gmail.com>
2019-04-28 01:56:23 +0200

    Item models moved to separated app.

--- artifical/admin.py
+++ artifical/admin.py
@@ -5,5 +5,4 @@ from artifical import models
 admin.site.register(models.location.Location)
 admin.site.register(models.mob.Mob)
 admin.site.register(models.mob.MobClass)
-admin.site.register(models.item.TemporaryItem)
-admin.site.register(models.item.HeroItem)
+
